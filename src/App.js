50f0e13dc3d17f374cd7151684cc1dc763ea4883
Eric Boudreaux <99383061+EricBoudreaux@users.noreply.github.com>
2022-08-30 00:24:43 -0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,48 +1,23 @@
-import React, { useState, useEffect } from 'react';
-import { Dimmer, Loader } from 'semantic-ui-react';
-import Weather from './components/weather'
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
-
-  const [lat, setLat] = useState([])
-  const [long, setLong] = useState([])
-  const [data, setData] = useState([])
-
-  useEffect(() => {
-    const fetchData = async () => {
-    navigator.geolocation.getCurrentPosition(function(position){
-      setLat(position.coords.latitude)
-      setLong(position.coords.longitude)
-    });
-    
-    await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=3e95e0f0bc9584beee7c51d87dfb27ca`)
-    .then(res => res.json())
-    .then(result => {
-      setData(result)
-      console.log(result);
-    });
-  }
-  fetchData()
-  }, [lat, long])
-  
-
-  
-
   return (
-
-    
     <div className="App">
-      {(typeof data.main != 'undefined') ? (
-        <Weather weatherData={data}/>
-      ): (
-        <div>
-          <Dimmer active>
-            <Loader>Loading...</Loader>
-          </Dimmer>
-        </div>
-      )}
-      
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
